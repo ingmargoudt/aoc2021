7f8cd297c97c8cb4eda6c021cfb9d979403cdbf5
igoudt <igoudt@bol.com>
2021-12-06 11:48:35 +0100

    add day6

---
+++ day6/day6.py
@@ -0,0 +1,18 @@
+with open("day6/day6.in.txt") as input:
+        p =  [int(p) for p in [(line.split(',')) for line in input][0]]
+        population = {x:p.count(x) for x in p}
+print(population)
+DAYS = 256
+currentday = 0
+while currentday < DAYS:
+    new_population = dict.fromkeys(list(range(0,8)), 0)
+    for state, amount in population.items():
+        if(state == 0):
+            new_population[6] += amount
+            new_population[8] = amount
+        else:
+            new_population[state-1] += amount
+    population = new_population
+    currentday+=1
+    print('After {} day{}: {}'.format(currentday, 's' if currentday > 1 else '',new_population))
+print(sum(population.values()))
